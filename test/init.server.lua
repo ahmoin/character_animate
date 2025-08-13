7b358e9dc5862696810bb1ac761c945c44d105a5
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-13 19:23:52 -0400

    feat: switch to rojo

--- test/init.server.lua
+++
@@ -1,9 +0,0 @@
-local StarterPlayer = game:GetService("StarterPlayer")
-local StarterCharacterScripts = StarterPlayer:WaitForChild("StarterCharacterScripts")
-
-local Animate = script:WaitForChild("Animate")
-
-local found = StarterCharacterScripts:FindFirstChild(Animate.Name)
-if not found then
-	Animate:Clone().Parent = StarterCharacterScripts
-end
